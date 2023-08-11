87a1002d92cac5a4d0fc84344ff823bcd9552d6c
Harkirat Singh <101324660+Harkiratpb11@users.noreply.github.com>
2023-08-11 13:56:44 +0530

    Create Kth_element.java

---
+++ Kth_element.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public static void main(String[] args){
+  Scanner sc=new Scanner(System.in);
+  int n=sc.newInt();
+  int[] arr=new arr[n];
+  for(int i=0;i<n;i++){
+    arr[i]=sc.nextInt();
+  }
+  System.out.println("the position of element to print :");
+  int key=sc.nextInt();
+  Arrays.sort(arr);
+  System.out.println(arr[n-k]);
+}
